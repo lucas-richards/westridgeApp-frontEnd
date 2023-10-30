e634d78ac4db26e672c265426538ba613ab0b3d7
lucas richards dev <lucasrichardsdev@gmail.com>
2023-10-30 14:59:13 -0700

    image of pilot plate

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -90,7 +90,7 @@ export default function Navbar() {
                                 >
                                 Home
                             </Link>
-                            <Link 
+                            {/* <Link 
                                 href="/guestbook" 
                                 prefetch 
                                 className={`${pathname == "/guestbook"
@@ -98,7 +98,7 @@ export default function Navbar() {
                                 :"border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-mainColor dark:hover:text-mainColor block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"}`}
                                 >
                                 Guestbook
-                            </Link>
+                            </Link> */}
                             <Link 
                                 href="/projects" 
                                 prefetch 
